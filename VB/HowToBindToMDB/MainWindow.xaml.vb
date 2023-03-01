ae11d6c8d61baa56ba54bfce825964b03900befe
DevExpressExampleBot <fakeEmail>
2023-03-01 11:35:19 +0400

    Source auto update [skip ci]

--- VB/HowToBindToMDB/MainWindow.xaml.vb
+++ VB/HowToBindToMDB/MainWindow.xaml.vb
@@ -1,29 +1,28 @@
-﻿Imports Microsoft.VisualBasic
-Imports System.Data
-Imports System.Data.OleDb
 Imports System.Windows
 Imports DevExpress.Xpf.PivotGrid
 Imports HowToBindToMDB.NwindDataSetTableAdapters
 Imports System
 
 Namespace HowToBindToMDB
-    Partial Public Class MainWindow
+
+    Public Partial Class MainWindow
         Inherits Window
-        Private dataTable As New NwindDataSet.OrderDetailsDataTable()
-        Private dataAdapter As New OrderDetailsTableAdapter()
+
+        Private dataTable As NwindDataSet.OrderDetailsDataTable = New NwindDataSet.OrderDetailsDataTable()
+
+        Private dataAdapter As OrderDetailsTableAdapter = New OrderDetailsTableAdapter()
 
         Public Sub New()
-            InitializeComponent()
-            pivotGridControl1.DataSource = dataTable
+            Me.InitializeComponent()
+            Me.pivotGridControl1.DataSource = dataTable
         End Sub
 
         Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
             dataAdapter.Fill(dataTable)
         End Sub
 
-        Private Sub pivotGridControl1_CustomUnboundFieldData(ByVal sender As Object, _
-                                                             ByVal e As PivotCustomFieldDataEventArgs)
-            If e.Field Is fieldTotalSum Then
+        Private Sub pivotGridControl1_CustomUnboundFieldData(ByVal sender As Object, ByVal e As PivotCustomFieldDataEventArgs)
+            If e.Field Is Me.fieldTotalSum Then
                 Dim unitPrice As Decimal = Convert.ToDecimal(e.GetListSourceColumnValue("UnitPrice"))
                 Dim qty As Integer = Convert.ToInt32(e.GetListSourceColumnValue("Quantity"))
                 Dim discount As Decimal = Convert.ToDecimal(e.GetListSourceColumnValue("Discount"))
