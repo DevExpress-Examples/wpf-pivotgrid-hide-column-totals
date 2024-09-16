5cc2f1ed5a115f6378815b965d220f90f2fa082c
Natalia Kazakova <natalia.kazakova@devexpress.com>
2024-09-16 19:08:31 +0200

    update example to .net8

--- CS/HowToBindToMDB/MainWindow.xaml.cs
+++ CS/HowToBindToMDB/MainWindow.xaml.cs
@@ -1,21 +1,44 @@
-﻿using System.Windows;
-using DevExpress.Xpf.PivotGrid;
-using System;
+﻿using DevExpress.Xpf.PivotGrid;
+using System.Data;
+using System.Windows;
+using Microsoft.Data.Sqlite;
 
 namespace UnboundFieldExample {
     public partial class MainWindow : Window {
-        DataSet1.OrderDetailsDataTable dataTable = new DataSet1.OrderDetailsDataTable();
-        DataSet1TableAdapters.OrderDetailsTableAdapter dataAdapter = new DataSet1TableAdapters.OrderDetailsTableAdapter();
-
         public MainWindow() {
             InitializeComponent();
-            pivotGridControl1.DataSource = dataTable;
         }
+        private void Window_Loaded(object sender,RoutedEventArgs e) {
+            // Create a data source.
+            DataTable dt;
 
-        private void Window_Loaded(object sender, RoutedEventArgs e) {
-            dataAdapter.Fill(dataTable);
+            using (SqliteConnection connection = new SqliteConnection("Data Source=nwind.db;")) {
+                connection.Open();
+                SqliteCommand cmd = (SqliteCommand)SqliteFactory.Instance.CreateCommand();
+                cmd.CommandText = "SELECT * FROM OrderDetails\r\nINNER JOIN Products ON Products.ProductID = OrderDetails.ProductID";
+                cmd.Connection = connection;
+                using (SqliteDataReader dr = cmd.ExecuteReader()) {
+                    do {
+                        dt = new DataTable();
+                        dt.BeginLoadData();
+                        dt.Load(dr);
+                        dt.EndLoadData();
+
+                    } while (!dr.IsClosed && dr.NextResult());
+                }
+            }
+
+            // Assign the data source to the PivotGrid control.
+            pivotGridControl1.DataSource = dt;
         }
-        private void PivotGridControl1_CustomCellValue(object sender, PivotCellValueEventArgs e) {
+        private void AddField(string caption,FieldArea area,string columnName,int index) {
+            PivotGridField field = pivotGridControl1.Fields.Add();
+            field.Caption = caption;
+            field.Area = area;
+            field.DataBinding = new DataSourceColumnBinding(columnName);
+            field.AreaIndex = index;
+        }
+        private void PivotGridControl1_CustomCellValue(object sender,PivotCellValueEventArgs e) {
             if (e.ColumnValueType == FieldValueType.GrandTotal
                    && e.DataField == fieldDiscount)
                 e.Value = null;
--- VB/HowToBindToMDB/MainWindow.xaml.vb
+++ VB/HowToBindToMDB/MainWindow.xaml.vb
@@ -1,19 +1,38 @@
 ﻿Imports System.Windows
 Imports DevExpress.Xpf.PivotGrid
 Imports System
+Imports Microsoft.Data.Sqlite
 
 Namespace UnboundFieldExample
 	Partial Public Class MainWindow
 		Inherits Window
-		Private dataTable As New DataSet1.OrderDetailsDataTable()
-		Private dataAdapter As New DataSet1TableAdapters.OrderDetailsTableAdapter()
 		Public Sub New()
 			InitializeComponent()
-			pivotGridControl1.DataSource = dataTable
 		End Sub
 		Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			dataAdapter.Fill(dataTable)
+			' Create a data source.
+			Dim dt As DataTable
+
+			Using connection As New SqliteConnection("Data Source=nwind.db;")
+				connection.Open()
+				Dim cmd As SqliteCommand = CType(SqliteFactory.Instance.CreateCommand(), SqliteCommand)
+				cmd.CommandText = "SELECT * FROM OrderDetails" & vbCrLf & "INNER JOIN Products ON Products.ProductID = OrderDetails.ProductID"
+				cmd.Connection = connection
+				Using dr As SqliteDataReader = cmd.ExecuteReader()
+					Do
+						dt = New DataTable()
+						dt.BeginLoadData()
+						dt.Load(dr)
+						dt.EndLoadData()
+
+					Loop While Not dr.IsClosed AndAlso dr.NextResult()
+				End Using
+			End Using
+
+			' Assign the data source to the PivotGrid control.
+			pivotGridControl1.DataSource = dt
 		End Sub
+
 		Private Sub PivotGridControl1_CustomCellValue(ByVal sender As Object, ByVal e As PivotCellValueEventArgs)
 			If e.ColumnValueType = FieldValueType.GrandTotal AndAlso e.DataField Is fieldDiscount Then
 				e.Value = Nothing
